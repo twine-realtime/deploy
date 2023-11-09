a9a7cdb0f241ab0a74308bc743762c97d145866c
Spencer Wightman <spencer.wightman@gmail.com>
2023-11-09 16:46:00 +1300

    removed commander; improved validation

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-import { Command } from 'commander';
 import { v4 as uuid4 } from 'uuid';
 import { readFileSync } from 'fs';
 import { CloudFormationClient, CreateStackCommand, Capability } from "@aws-sdk/client-cloudformation";
@@ -6,115 +5,162 @@ import { EC2Client, DescribeRegionsCommand } from "@aws-sdk/client-ec2";
 import { fromIni } from "@aws-sdk/credential-provider-ini";
 const inquirer = require('inquirer');
 
-const program = new Command();
-
-program
-  .name('Twine CLI')
-  .description('Deploy Twine with AWS CloudFormation')
-  .version('0.1.0');
-
-  program
-  .command('deploy')
-  .description('Interactively deploy Twine')
-  .action(async () => {
-    const questions = [
-      {
-        type: 'input',
-        name: 'profile',
-        message: 'AWS CLI profile name for credentials and settings:',
-        validate: (input: string) => input.length > 0 ? true : "Profile name cannot be empty."
-      },
-      {
-        type: 'input',
-        name: 'region',
-        message: 'AWS region for deployment:',
-        validate: (input: string) => input.length > 0 ? true : "Region cannot be empty."
-      },
-      {
-        type: 'input',
-        name: 'certificateArn',
-        message: 'ARN of the ACM TLS certificate for the Load Balancer:',
-        validate: (input: string) => input.length > 0 ? true : "Certificate ARN cannot be empty."
+const promptProfile = async () => {
+  let profile = '';
+  const question = {
+    type: 'input',
+    name: 'profile',
+    prefix: 'Twine ~',
+    message: 'AWS CLI profile name:',
+    validate: async (input: string) => {
+      if (input.length === 0) {
+        return "Profile name cannot be empty.";
       }
-      // add one for domain name, then use below
-    ];
+      try {
+        // Try to load the credentials to check if the profile exists
+        await fromIni({ profile: input })();
+        return true;
+      } catch (error) {
+        // If an error occurs, the profile does not exist
+        return `The specified profile "${input}" does not exist or could not be loaded.`;
+      }
+    }
+  };
 
-    const answers = await inquirer.prompt(questions);
-    const { profile, region, certificateArn } = answers;
-
-    const ec2Client = new EC2Client({ region: "us-east-1" }); // Default to a common region to fetch list of regions
-
+  // Keep prompting until valid input is received
+  while (profile === '') {
     try {
+      const answer = await inquirer.prompt(question);
+      profile = answer.profile;
+    } catch (error) {
+      console.error('An error occurred while validating the AWS profile:', error);
+      // If an error occurs during validation, the profile variable stays empty and the loop continues
+    }
+  }
+  return profile;
+};
+
+const promptRegion = async () => {
+  let region = '';
+  const question = {
+    type: 'input',
+    name: 'region',
+    prefix: 'Twine ~',
+    message: 'AWS region for deployment:',
+    // The region validation is now handled in the prompt itself
+    validate: async (input: string) => {
+      if (input.length === 0) {
+        return "Region cannot be empty.";
+      }
+
+      // Instantiate an EC2 client to verify the region
+      const ec2Client = new EC2Client({ region: "us-east-1" }); // Default to a common region to fetch list of regions
       const regionsResult = await ec2Client.send(new DescribeRegionsCommand({}));
       const validRegions = regionsResult.Regions?.map(r => r.RegionName) ?? [];
-
-      // Check if the supplied region is a current AWS region
-      if (!validRegions.includes(region)) {
-        console.error(`The specified region "${region}" is not a valid AWS region.`);
-        process.exit(1);
-      }
-
-      // Check if the certificate ARN includes the supplied region
-      if (!certificateArn.includes(region)) {
-        console.error(`The certificate ARN must be from the same region as the specified region: ${region}`);
-        process.exit(1);
-      }
-
-      const cloudFormationClient = new CloudFormationClient({
-        region: region,
-        credentials: fromIni({ profile }),
-      });
-
-      let templateContent;
-      try {
-        const templateBody = './templates/cloudformation.yaml';
-        templateContent = readFileSync(templateBody, 'utf8');
-      } catch (err: unknown) {
-        if (err instanceof Error) {
-          console.error(`Failed to read CloudFormation template: ${err.message}`);
-        } else {
-          console.error('An unknown error occurred while reading the CloudFormation template');
-        }
-        process.exit(1);
-      }
-
-      const cfParams = {
-        Parameters: [
-          {
-            ParameterKey: 'ACMCertificateARN',
-            ParameterValue: certificateArn,
-          },
-          {
-            ParameterKey: 'GeneratedApiKey',
-            ParameterValue: uuid4(),
-          },
-          {
-            ParameterKey: 'EnvironmentRegion',
-            ParameterValue: region,
-          },
-        ],
-        TemplateBody: templateContent,
-        Capabilities: [
-          Capability.CAPABILITY_IAM,
-          Capability.CAPABILITY_NAMED_IAM
-        ],
-        StackName: 'TwineStack'
-      };
-
-      try {
-        console.log(`Deploying Twine stack to region ${region}...`);
-        const createStackCommand = new CreateStackCommand(cfParams);
-        const stackResult = await cloudFormationClient.send(createStackCommand);
-        console.log(`Stack creation initiated, StackId: ${stackResult.StackId}`);
-      } catch (cfError) {
-        console.error('Error creating AWS CloudFormation stack:', cfError);
-        process.exit(1);
-      }
-
-    } catch (error) {
-      console.error('Error fetching AWS regions:', error);
-      process.exit(1);
+      return validRegions.includes(input) ? true : `The specified region "${input}" is not a valid AWS region.`;
     }
-  });
+  };
 
-program.parse(process.argv);
+  // Keep prompting until valid input is received
+  while (region === '') {
+    const answer = await inquirer.prompt(question);
+    region = answer.region;
+  }
+  return region;
+};
+
+const validateCertificateArn = (input: string, region: string) => {
+  const arnRegex = /^arn:aws:acm:[a-z0-9-]+:\d{12}:certificate\/[a-zA-Z0-9-]+$/;
+
+  if (input.length === 0) {
+    return "Certificate ARN cannot be empty.";
+  }
+
+  if (!arnRegex.test(input)) {
+    return "The ARN format is invalid.";
+  }
+
+  if (!input.includes(`:acm:${region}:`)) {
+    return `The certificate ARN must be from the same region as the deployment region: ${region}`;
+  }
+
+  return true;
+};
+
+const promptCertificateArn = async (region: string) => {
+  let certificateArn = '';
+  const question = {
+    type: 'input',
+    name: 'certificateArn',
+    prefix: 'Twine ~',
+    message: 'ARN of the ACM TLS certificate:',
+    validate: (input: string) => validateCertificateArn(input, region)
+  };
+
+  // Keep prompting until valid input is received
+  while (certificateArn === '') {
+    const answer = await inquirer.prompt(question);
+    certificateArn = answer.certificateArn;
+  }
+  return certificateArn;
+};
+
+(async () => {
+  try {
+    const profile = await promptProfile();
+    const region = await promptRegion();
+    const certificateArn = await promptCertificateArn(region);
+
+    const cloudFormationClient = new CloudFormationClient({
+      region: region,
+      credentials: fromIni({ profile }),
+    });
+
+    let templateContent;
+    try {
+      const templateBody = './templates/cloudformation.yaml';
+      templateContent = readFileSync(templateBody, 'utf8');
+    } catch (err: unknown) {
+      if (err instanceof Error) {
+        console.error(`Failed to read CloudFormation template: ${err.message}`);
+      } else {
+        console.error('An unknown error occurred while reading the CloudFormation template');
+      }
+    }
+
+    const cfParams = {
+      Parameters: [
+        {
+          ParameterKey: 'ACMCertificateARN',
+          ParameterValue: certificateArn,
+        },
+        {
+          ParameterKey: 'GeneratedApiKey',
+          ParameterValue: uuid4(),
+        },
+        {
+          ParameterKey: 'EnvironmentRegion',
+          ParameterValue: region,
+        },
+      ],
+      TemplateBody: templateContent,
+      Capabilities: [
+        Capability.CAPABILITY_IAM,
+        Capability.CAPABILITY_NAMED_IAM
+      ],
+      StackName: 'TwineStack'
+    };
+
+    try {
+      console.log(`Deploying Twine stack to region ${region}...`);
+      const createStackCommand = new CreateStackCommand(cfParams);
+      const stackResult = await cloudFormationClient.send(createStackCommand);
+      console.log(`Stack creation initiated, StackId: ${stackResult.StackId}`);
+    } catch (cfError) {
+      console.error('Error creating AWS CloudFormation stack:', cfError);
+    }
+
+  } catch (error) {
+    console.error('An error occurred:', error);
+  }
+})();
